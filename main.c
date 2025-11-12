ed0b2a78b6942aa9b02a4b92e44cce7e7cad3788
Samuel Stegall <swstegall@gmail.com>
2025-11-12 17:43:15 -0600

    Added support for transparency.

--- main.c
+++ main.c
@@ -19,25 +19,32 @@ static void on_drag_begin(GtkGestureClick *gesture,
 
 static void activate(GtkApplication *app, gpointer user_data) {
     GtkWidget *window = gtk_application_window_new(app);
-    GtkWidget *web_view = webkit_web_view_new();
-
     gtk_window_set_title(GTK_WINDOW(window), "Borderless Draggable WebView");
     gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
     gtk_window_set_decorated(GTK_WINDOW(window), FALSE);
     gtk_window_set_resizable(GTK_WINDOW(window), TRUE);
 
+    // Make the entire window (including all children) 80% opaque.
+    // This is the correct, compositor-friendly way in GTK4 for whole-window translucency.
+    gtk_widget_set_opacity(window, 0.8);
+
+    // WebView
+    GtkWidget *web_view = webkit_web_view_new();
+
+    // Make WebView background transparent so window background shows through
+    GdkRGBA transparent = {0, 0, 0, 0};
+    webkit_web_view_set_background_color(WEBKIT_WEB_VIEW(web_view), &transparent);
+
     webkit_web_view_load_uri(WEBKIT_WEB_VIEW(web_view), "https://www.example.com");
 
-    // Gesture controller for drag-to-move
+    // Drag-to-move
     GtkGesture *drag = gtk_gesture_click_new();
     gtk_event_controller_set_propagation_phase(GTK_EVENT_CONTROLLER(drag), GTK_PHASE_BUBBLE);
     gtk_gesture_single_set_button(GTK_GESTURE_SINGLE(drag), GDK_BUTTON_PRIMARY);
     g_signal_connect(drag, "pressed", G_CALLBACK(on_drag_begin), window);
-
     gtk_widget_add_controller(web_view, GTK_EVENT_CONTROLLER(drag));
-    gtk_window_set_child(GTK_WINDOW(window), web_view);
 
-    // GTK 4 prefers gtk_window_present() over gtk_widget_show()
+    gtk_window_set_child(GTK_WINDOW(window), web_view);
     gtk_window_present(GTK_WINDOW(window));
 }
 
